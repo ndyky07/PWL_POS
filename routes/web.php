27ee5269673cd5e915172c56a2016449805d5b2e
ndyky07 <sandipamungkas600@gmail.com>
2025-04-14 18:20:20 +0700

    pratikum 4 js 3

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,3 +20,4 @@ Route::get('/', function () {
 });
 
 route::get('/level', [LevelController::class, 'index']);
+route::get('kategori', [KategoriController::class, 'index']);
