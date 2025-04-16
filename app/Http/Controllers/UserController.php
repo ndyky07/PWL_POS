83bda3045b4dc45f0b6b4bf70ff5bd742ac85d73
ndyky07 <sandipamungkas600@gmail.com>
2025-04-16 14:28:15 +0700

    Praktikum 2.7 js 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,8 +10,8 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);
+        $user = UserModel::with('level')->get();
+        return view( 'user',['data'=> $user]);
     }
 
     public function tambah()
@@ -55,7 +55,7 @@ class UserController extends Controller
     { 
         $user = UserModel::find($id); 
         $user->delete(); 
-        
+
         return redirect('/user'); 
     }
     
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 use function Laravel\Prompts\password;
 
@@ -13,10 +15,16 @@ class UserModel extends Model
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = ['level_id', 'username', 'nama','password'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
 }
