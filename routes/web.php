4856e13735dcd8f14d760c31bca35b952af84955
ndyky07 <sandipamungkas600@gmail.com>
2025-04-16 16:05:28 +0700

    Praktikum 2 js 5

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,10 +22,13 @@ Route::get('/', function () {
 });
 
 route::get('/level', [LevelController::class, 'index']);
-route::get('kategori', [KategoriController::class, 'index']);
-Route::get('/user', [UserController::class, 'index']);
+route::get('/kategori', [KategoriController::class, 'index']);
+route::get('/user', [UserController::class, 'index']);
+
 Route::get('/user/tambah', [UserController::class, 'tambah']);
 Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']); 
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
 Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']); 
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+
+Route::get('/', [WelcomeController::class,'index']);
